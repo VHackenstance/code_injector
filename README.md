0c0ca481f9788d8bea87cbcbf102566769f6d3a5
Vhackenstance <hackenstance@proton.me>
2026-04-13 06:29:59 -0500

    Convert replace_download to print HTTP Requests and Responses.

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
-<h1>CODE INJECTOR</h1>
+<h3>CODE INJECTOR</h3>
+<h4>Process - Locally</h4>
+<ol>
+<li>
+    <b>Run IP Table rules</b>
+sudo iptables -I INPUT -j NFQUEUE --queue-num 0 --queue-bypass
+<br/>
+sudo iptables -I OUTPUT -j NFQUEUE --queue-num 0 --queue-bypass
+</li>
+<li></li>
+<li></li>
+</ol>
 <p></p>
 <p></p>
 <p></p>
@@ -7,8 +18,4 @@
 <h3>
 </h3>
 <h4>/h4>
-<ol>
-<li></li>
-<li></li>
-<li></li>
-</ol>
+
--- code_injector.py
+++ code_injector.py
@@ -1,5 +1,29 @@
 #!/usr/bin/env python3
+import netfilterqueue
+from scapy.layers.inet import IP, TCP
+from scapy.layers.dns import Raw
 
+new_line = "\n\n"
+def set_load(packet, load):
+    packet[Raw].load = load
+    del packet[IP].len
+    del packet[IP].chksum
+    del packet[TCP].chksum
+    return packet
 
-if __name__ == "__main__":
-    print("Hello World, this is Code Injector")
+def process_packet(packet):
+    scapy_packet= IP(packet.get_payload())
+    if scapy_packet.haslayer(Raw) and scapy_packet.haslayer(TCP):
+        if scapy_packet[TCP].dport == 80:
+            print("[+] This is an HTTP Request: ")
+            print(scapy_packet.show())
+        elif scapy_packet[TCP].sport == 80:
+            print("This is a *** HTTP Response ***: ")
+            print(scapy_packet.show())
+    packet.accept()
+    return None
+
+print("Hello World, this is Code Injector")
+queue = netfilterqueue.NetfilterQueue()
+queue.bind(0, process_packet)
+queue.run()
