bdbca2c4aea331ae11a54e7ec8330a7d0715525b
Vhackenstance <hackenstance@proton.me>
2026-06-08 11:28:54 -0500

    Nice spot to test removal of Accept-Encoding

--- README.md
+++ README.md
@@ -14,22 +14,19 @@ HTTP only.
         <b>sudo iptables -L</b>
     </li>
     <li>
-   
+        We want to remove Accept-Encoding from the HTTP Request load.
+        <br />
+        <b>Accept-Encoding: gzip, deflate\r\n </b>
+        <li>
+            Do this with regex: <b>"Accept-Encoding:.*?\\r\\n"</b>
+        </li>
     </li>
 </ol>
 <p>I am testing against.<br/>
 <b>http://testasp.vulnweb.com/</b><br/>
 /var/www/htlm/index.html
 </p>
-<h4>OWASP Webgoat Install, for (Kali?) Linux only.</h4>
-<p>I am yet to test this <b>Locally</b> against packet_sniffer and replace_downloads
-    <br/>
-    But I am hoping it will do a better job than OWASP Juice shop - locally.
-    <br/>
-    Oh, and no Docker, sorry not sorry.  Really do not like it, almost exclusively because of the 
-    way it just vomits all over ip_tables.  If it is doing that to ip_tables how is
-    is messing up other parts of your code?
-</p>
+    <h4>OWASP Webgoat Install.  Tested locally does not generate HTTP requests or responses</h4>
 <ol>
 <li>Navigate to: <b>https://github.com/WebGoat/WebGoat/releases</b></li>
 <b>Download the <b>webgoat-[version_number].jar</b> file</li>
--- code_injector.py
+++ code_injector.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python3
-# Rebuild - initially copy all code from replace_download main.py
+# Rebuild
 import netfilterqueue
 from scapy.layers.inet import IP, TCP
 from scapy.layers.dns import Raw
+
+import re
+
+AcceptEncodingRegex = "Accept-Encoding:.*?\\r\\n"
 
 def process_packet(packet):
     scapy_packet= IP(packet.get_payload())
@@ -11,11 +15,16 @@ def process_packet(packet):
             # print("\n[+] Packet has layer TCP")
             if scapy_packet[TCP].dport == 80:
                 print("[+] This is a HTTP Request:  ")
-                print(scapy_packet.show())
+                print("[+] This is our current load with Accept Encoding:  ")
+                print(scapy_packet[Raw].load)
+                # Find string Accept-Encoding in HTTPR Raw layer load
+                # replace with nothing and as as modified_load
+                modified_load = re.sub(AcceptEncodingRegex, "", scapy_packet[Raw].load)
+                print("[+] Load minus the Accept Encoding:  ")
+                print(modified_load)
 
-            elif scapy_packet[TCP].sport == 80:
-                print("[+] This is a HTTP Response: ")
-                print(scapy_packet.show())
+            # elif scapy_packet[TCP].sport == 80:
+                # print("[+] This is a HTTP Response: ")
 
     packet.accept()
 
